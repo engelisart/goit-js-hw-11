332fb1566b608814b25251f9ea9dc1a6b67daee6
engelisart <144841027+engelisart@users.noreply.github.com>
2023-11-16 16:27:11 +0100

    updated js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-
+import { PicturesAPI } from './pictures-api';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
+
+const refs = {
+  formElem: document.querySelector('.js-form'),
+  inputElem: document.querySelector('.js-input'),
+  btnLoadMore: document.querySelector('.js-load-more'),
+  gallery: document.querySelector('.js-gallery'),
+};
+
+const picturesAPI = new PicturesAPI();
+
+
+refs.formElem.addEventListener('submit', onFormElemSubmit);
+refs.inputElem.addEventListener('change', onInputElemChange);
+
+function onFormElemSubmit(event) {
+  event.preventDefault();
+  picturesAPI.fetchPhotosByQuery().then(data=>{
+    console.log()
+    data.hits.forEach((d)=>{
+        refs.gallery.innerHTML += pictureElements(d)
+    })
+  })
+  
+}
+
+function onInputElemChange(event) {
+  event.preventDefault();
+
+  console.log('hello!');
+}
+
+function pictureElements({
+  wedformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `<div class="photo-card">
+<img src="${wedformatURL}" alt="${tags}" loading="lazy" />
+<div class="info">
+  <p class="info-item">
+    <b>${likes}</b>
+  </p>
+  <p class="info-item">
+    <b>${views}</b>
+  </p>
+  <p class="info-item">
+    <b>${comments}</b>
+  </p>
+  <p class="info-item">
+    <b>${downloads}</b>
+  </p>
+</div>
+</div>`;
+}
--- src/pictures-api.js
+++ src/pictures-api.js
@@ -1,11 +1,28 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com/api';
+export class PicturesAPI {
+  constructor() {
+    this.page = 1;
 
-const axiosOptions = {
-    params: {
-        image_type: '',
-       page: 1,
-       per_page: 20, 
-    }
+    axios.defaults.baseURL = 'https://pixabay.com/api';
+  }
+
+  fetchPhotosByQuery() {
+    const axiosOptions = {
+      params: {
+        page: this.page,
+        per_page: 20,
+        key: '40691012-8f236ebf8c6cb98d313bec4db',
+        q: 'flower',
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+      },
+    };
+
+    return axios
+      .get('', axiosOptions)
+      .then(res => res.data)
+      .catch(err => console.log(err));
+  }
 }
